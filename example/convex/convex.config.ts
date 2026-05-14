c259f3cd340e386fcf51cc31badf8e57f545b91f
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-05-13 17:34:48 -0700

    host app under /api

--- example/convex/convex.config.ts
+++ example/convex/convex.config.ts
@@ -1,7 +1,7 @@
 import { defineApp } from "convex/server";
 import staticHosting from "@convex-dev/static-hosting/convex.config.js";
 
-const app = defineApp({ httpPrefix: "/app" });
+const app = defineApp({ httpPrefix: "/api" });
 app.use(staticHosting, { httpPrefix: "/" });
 
 export default app;
